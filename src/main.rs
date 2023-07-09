49335e35a04bc734600bc0b8b4338866426de7ef
Daniel Herman <daniel.herman@protonmail.com>
2023-07-09 15:22:52 +0200

    Making it sound more like TempleOS

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,48 @@ use core::time::Duration;
 use rodio::{OutputStream, source::Source};
 use clap::{arg, command};
 
-fn complexwave(frequency: f64) -> impl Fn(f64) -> f64 {
-    move |t| {
-        let mut amp = sine_wave(frequency)(t);
-        for i in 2..10 {
-            amp += sine_wave(2_i32.pow(i) as f64 * frequency)(t) / i as f64 * 1.0 / 1.3_f64.powf(i as f64);
+// generated with generate_templeos_pattern()
+static TEMPLEOS_FREQS: [(f64, f64); 40] = [(0.0, 0.7914783671016566), (-0.147540984, 0.7914783671016566), (0.147540984, 0.7914783671016566), (-0.27868852499999996, 0.7914783671016566), (0.27868852499999996, 0.7914783671016566), (-0.409836066, 0.7914783671016566), (0.409836066, 0.7914783671016566), (-0.540983607, 0.7914783671016566), (0.540983607, 0.7914783671016566), (-0.6721311480000001, 0.7914783671016566), (0.6721311480000001, 0.7914783671016566), (-0.8032786890000001, 0.7914783671016566), (0.8032786890000001, 0.7914783671016566), (-0.9344262300000001, 0.7914783671016566), (0.9344262300000001, 0.7914783671016566), (2.0, 0.5800207659791673), (1.852459016, 0.5800207659791673), (2.147540984, 0.5800207659791673), (1.721311475, 0.5800207659791673), (2.2786885249999997, 0.5800207659791673), (1.590163934, 0.5800207659791673), (2.409836066, 0.5800207659791673), (1.459016393, 0.5800207659791673), (2.5409836070000003, 0.5800207659791673), (1.327868852, 0.5800207659791673), (2.672131148, 0.5800207659791673), (1.196721311, 0.5800207659791673), (2.803278689, 0.5800207659791673), (1.0655737699999999, 0.5800207659791673), (2.93442623, 0.5800207659791673), (4.0, 0.4758686316592648), (3.852459016, 0.4758686316592648), (4.147540984, 0.4758686316592648), (3.7213114750000003, 0.4758686316592648), (4.278688525, 0.4758686316592648), (4.409836066, 0.4758686316592648), (4.540983607, 0.4758686316592648), (4.672131148, 0.4758686316592648), (4.803278689, 0.4758686316592648), (4.93442623, 0.4758686316592648)];
+
+fn generate_templeos_pattern() -> Vec<(f64, f64)> {
+    let decay_constant = 0.19999992;
+    let c0 = 0.43407478;
+    let c1 = 0.45959631;
+    let mut freqs = Vec::new();
+    let mut base_freq = 0.0;
+    for i in 1..=3 {
+        let mut add_freq = 0.147540984;
+        let max_amplitude = 1.0 - decay_constant * (i - 1) as f64;
+        let mut amplitudes = Vec::new();
+        for _ in 0..=7 {
+            amplitudes.push((-c1 * i as f64).exp() * (max_amplitude - c0) + c0);
         }
-        amp
+        freqs.push((base_freq, amplitudes[0]));
+        for j in 1..=7 {
+            for sign in [-1.0, 1.0] {
+                if sign == -1.0 && j >2 && i == 3 {
+                    continue;
+                }
+                let freq = base_freq + sign * add_freq;
+                let amplitude = amplitudes[j];
+
+                freqs.push((freq, amplitude));
+            }
+            add_freq += 0.131147541
+        }
+        base_freq += 2.0;
+    }
+    freqs
+}
+
+fn complexwave(frequency: f64) -> impl Fn(f64) -> f64 {
+    let freqs = TEMPLEOS_FREQS;
+    move |t: f64| -> f64 {
+        let mut sum = 0.0;
+        for (freq, amplitude) in freqs.iter() {
+            sum += amplitude * sine_wave(*freq * frequency)(t);
+        }
+        sum
     }
 }
 
@@ -59,12 +94,29 @@ impl Note {
             }
         })
     }    
+
+    fn create_templeos_faded_tone(&self, duration: f64, sample_rate: u32, tone_length: f64) -> Vec<f64> {
+        let frequency = self.to_frequency();
+        let pause_length = 1.0 - tone_length;
+        make_samples(duration, sample_rate as usize, |t: f64| -> f64 {
+            let amp = complexwave(frequency)(t);
+            if t < 2.0 * pause_length * duration {
+                0.5 * amp * (t / (2.0 * pause_length * duration))
+            } else if t < (1.0 - 2.0 * pause_length) * duration {
+                0.5 * amp
+            } else {
+                0.5 * amp * ((duration - t) / (2.0 * pause_length * duration))
+            }
+        })
+    }  
 }
 
 #[derive(Clone, Copy)]
+#[allow(dead_code)]
 enum Tone {
     Simple(Note, f64),
     Faded(Note, f64),
+    TempleOS(Note, f64),
 }
 
 struct Melody {
@@ -84,6 +136,10 @@ impl Melody {
                 },
                 Tone::Faded(note, duration) => {
                     let mut note_samples = note.create_faded_tone(*duration, self.sample_rate, self.tone_length);
+                    samples.append(&mut note_samples);
+                },
+                Tone::TempleOS(note, duration) => {
+                    let mut note_samples = note.create_templeos_faded_tone(*duration, self.sample_rate, self.tone_length);
                     samples.append(&mut note_samples);
                 },
             }
@@ -196,12 +252,14 @@ fn main() {
     melody_tones.extend_from_slice(&melody_first);
     melody_tones.extend_from_slice(&melody_second);
     melody_tones.extend_from_slice(&melody_second);
+    //  add one to octave
+    // melody_tones = melody_tones.iter().map(|(note, duration)| (Note { letter: note.letter, octave: note.octave + 1 }, *duration)).collect();
 
     let tone_length = 60.0 / tempo as f64 ;
     melody_tones = melody_tones.iter().map(|(note, duration)| (*note, *duration * tone_length)).collect();
 
     let melody = Melody {
-        melody: melody_tones.iter().map(|(note, duration)| Tone::Faded(*note, *duration)).collect(),
+        melody: melody_tones.iter().map(|(note, duration)| Tone::TempleOS(*note, *duration)).collect(),
         sample_rate: 44_100,
         tone_length: 0.98,
     };
